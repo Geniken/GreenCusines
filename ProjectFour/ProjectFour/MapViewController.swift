304b1b237cff241a8d84e40b2146fb01abeaf74d
Daniel Kim <Msdk5500@gmail.com>
2016-11-03 20:24:54 -0700

    Beta

--- ProjectFour/ProjectFour/MapViewController.swift
+++ ProjectFour/ProjectFour/MapViewController.swift
@@ -14,27 +14,6 @@ import Async
 
 class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
-    //    class farmerMarket:NSObject, MKAnnotation {
-    //
-    //        let title:String?
-    //        let locationName:String
-    //        let discipline: String
-    //        let coordinate:CLLocationCoordinate2D
-    //
-    //        init(title:String, locationName:String, discipline:String, coordinate:CLLocationCoordinate2D) {
-    //
-    //            self.title = title
-    //            self.locationName = locationName
-    //            self.discipline = discipline
-    //            self.coordinate = coordinate
-    //        }
-    //
-    //        var subtitle:String? {
-    //
-    //            return locationName
-    //        }
-    //    }
-    
     @IBOutlet weak var mapView: MKMapView!
     
     //User Location
@@ -95,25 +74,48 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     // Centering map around user
     
     func locationManager(_ manager:CLLocationManager, didUpdateLocations locations:[CLLocation]) {
+        
+        let userLocation:CLLocation = locations[0]
+        
+        let latitude = userLocation.coordinate.latitude
+        
+        let longitude = userLocation.coordinate.longitude
+        
+        let latDelta: CLLocationDegrees = 0.05
+        
+        let longDelta: CLLocationDegrees = 0.05
+        
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        
+        let location = CLLocationCoordinate2D(latitude:latitude, longitude:longitude)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        self.mapView.setRegion(region, animated:true)
+    }
+    
+    //Map Annotations
+    
+    class farmerMarket:NSObject, MKAnnotation {
+        
+        let title:String?
+        let locationName:String
+        let discipline: String
+        let coordinate:CLLocationCoordinate2D
+        
+        init(title:String, locationName:String, discipline:String, coordinate:CLLocationCoordinate2D) {
             
-            let userLocation:CLLocation = locations[0]
+            self.title = title
+            self.locationName = locationName
+            self.discipline = discipline
+            self.coordinate = coordinate
+        }
+        
+        var subtitle:String? {
             
-            let latitude = userLocation.coordinate.latitude
-            
-            let longitude = userLocation.coordinate.longitude
-            
-            let latDelta: CLLocationDegrees = 0.05
-            
-            let longDelta: CLLocationDegrees = 0.05
-            
-            let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
-            
-            let location = CLLocationCoordinate2D(latitude:latitude, longitude:longitude)
-            
-            let region = MKCoordinateRegion(center: location, span: span)
-            
-            self.mapView.setRegion(region, animated:true)
+            return locationName
         }
     }
+}
 
 
--- ProjectFour/ProjectFour/SearchTableViewController.swift
+++ ProjectFour/ProjectFour/SearchTableViewController.swift
@@ -55,8 +55,6 @@ class SearchTableViewController: UITableViewController {
     
     @IBAction func searchButton(_ sender: AnyObject) {
         
-        //        self.recipeChoices.removeAll()
-        
         // Load JSON data asynchronously
         
         Async.background {
@@ -87,6 +85,10 @@ class SearchTableViewController: UITableViewController {
                         let jsonResult = try? JSONSerialization.jsonObject(with: data, options: []) as! NSDictionary
                         
                         let array = jsonResult?["hits"] as? NSArray
+                        
+                        self.recipeChoices.removeAll()
+                        self.recipeName.removeAll()
+                        self.calories.removeAll()
                         
                         for recipeJSONEntry in array! {
                             
@@ -185,20 +187,20 @@ class SearchTableViewController: UITableViewController {
     
     
     // Slide in Animation for Images
-    
-    override func tableView(_ tableView:UITableView, willDisplay cell:UITableViewCell, forRowAt indexPath: IndexPath) {
-        
-        let rotationTransform = CATransform3DTranslate(CATransform3DIdentity, -300, 10, 0)
-        
-        cell.layer.transform = rotationTransform
-        
-        UIView.animate(withDuration: 0.5, animations:{ () -> Void in
-            
-            cell.layer.transform = CATransform3DIdentity
-            
-        })
-    }
-    
+//    
+//    override func tableView(_ tableView:UITableView, willDisplay cell:UITableViewCell, forRowAt indexPath: IndexPath) {
+//        
+//        let rotationTransform = CATransform3DTranslate(CATransform3DIdentity, -300, 10, 0)
+//        
+//        cell.layer.transform = rotationTransform
+//        
+//        UIView.animate(withDuration: 0.5, animations:{ () -> Void in
+//            
+//            cell.layer.transform = CATransform3DIdentity
+//            
+//        })
+//    }
+//    
     
     
     //Reusuable Table View Cell
@@ -209,11 +211,10 @@ class SearchTableViewController: UITableViewController {
             
             guard !recipeChoices.isEmpty else { return UITableViewCell() }
             
-            
             let recipeforRow = recipeChoices[indexPath.row]
             
             
-            guard let newData = try? Data (contentsOf:recipeforRow.url!) else {return UITableViewCell () }
+            guard let newData = try? Data (contentsOf:recipeforRow.pic!) else {return UITableViewCell () }
             guard let imageObject = UIImage(data:newData) else {return UITableViewCell () }
             
             Async.background {
